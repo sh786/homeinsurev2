c76e0e25e2bda73a1e495bb343f7e0b90f2c8121
Sam Hamburger <sh786@cornell.edu>
2018-04-19 14:12:46 -0400

    mock plans load thru react

--- src/components/SellInsure.js
+++ src/components/SellInsure.js
@@ -1,6 +1,78 @@
 import React, {Component} from 'react';
 
+const TableRow = ({row}) => (
+  <tr>
+    <td key={row.address}>{row.address}</td>
+    <td key={row.city}>{row.city}</td>
+    <td key={row.state}>{row.state}</td>
+    <td key={row.zip}>{row.zip}</td>
+    <td key={row.risk}>{row.risk}</td>
+    <td key={row.amountRemaining}>{row.amountRemaining}</td>
+    <td>
+      <div className="field">
+        <div className="control">
+          <input className="input" id="purchase" type="text" placeholder="ETH Purchase"/>
+        </div>
+      </div>
+    </td>
+    <td>
+      <div className="field">
+        <div className="control">
+          <button className="button is-link" id="confirm">
+            <span className="icon">
+              <i className="fas fa-paper-plane has-text-white"></i>
+            </span>
+          </button>
+        </div>
+      </div>
+    </td>
+  </tr>
+);
+
 export default class SellInsure extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      insurancePlans: []
+    }
+  }
+
+  componentWillMount() {
+    this.getInsurancePlans()
+  }
+
+  getInsurancePlans() {
+    // read from db mockdata
+    let mockPlans = [
+      {
+        id: 1,
+        address: '123 Main St.',
+        city: 'New York',
+        state: 'NY',
+        zip: '10003',
+        risk: 0.20,
+        amountRemaining: 10.3
+      }, {
+        id: 2,
+        address: '456 North St.',
+        city: 'Ithaca',
+        state: 'NY',
+        zip: '14850',
+        risk: 0.15,
+        amountRemaining: 4.2
+      }, {
+        id: 3,
+        address: '789 South St.',
+        city: 'Boxford',
+        state: 'MA',
+        zip: '01921',
+        risk: 0.08,
+        amountRemaining: 7.2
+      }
+    ]
+    this.setState({insurancePlans: mockPlans})
+  }
 
   render() {
     return (
@@ -19,32 +91,9 @@ export default class SellInsure extends Component {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>123 Main St.</td>
-              <td>New York</td>
-              <td>NY</td>
-              <td>10003</td>
-              <td>0.10</td>
-              <td>10.3 ETH</td>
-              <td>
-                <div className="field">
-                  <div className="control">
-                    <input className="input" id="purchase" type="text" placeholder="ETH Purchase"/>
-                  </div>
-                </div>
-              </td>
-              <td>
-              <div className="field">
-            <div className="control">
-              <button className="button is-link" id="confirm">
-              <span className="icon">
-                <i className="fas fa-paper-plane has-text-white"></i>
-              </span>
-              </button>
-            </div>
-          </div>
-              </td>
-            </tr>
+            {this.state.insurancePlans.map(row => {
+              return <TableRow key={row.id} row={row} />
+            })}
           </tbody>
         </table>
       </div>
