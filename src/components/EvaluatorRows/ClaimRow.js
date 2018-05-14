6c1bd32d6cc12044b4254a2a6e68364efdac9448
Sravan Reddy <sravanreddy@dhcp-rhodes-35.redrover.cornell.edu>
2018-05-14 13:50:32 -0400

    triggers metamask on claim

--- src/components/EvaluatorRows/ClaimRow.js
+++ src/components/EvaluatorRows/ClaimRow.js
@@ -35,7 +35,12 @@ export default class ClaimRow extends Component {
           .then((instance) => {
             this.state.myweb3.eth.defaultAccount = accounts[0]
             insuranceInstance = instance
-
+            return insuranceInstance.get_homeowner(_house_token, {from: accounts[0]})
+          })
+          .then((result) => {
+            this.updateUI()
+          })
+            /*
             console.log('Adding stake')
             return insuranceInstance.create_insurance_claim.call(
                 _house_token, _claim_amount, {from: accounts[0]})
@@ -43,10 +48,10 @@ export default class ClaimRow extends Component {
           .then((result) => {
             console.log('transaction completed')
             //if declined, true
-            console.log(result)
             return insuranceInstance.create_insurance_claim(
                 _house_token, _claim_amount, {from: accounts[0]})
           })
+          */
 
       })
   }
@@ -57,11 +62,46 @@ export default class ClaimRow extends Component {
     })
   }
 
+  updateUI() {
+    var updates = {}
+    var id = this.state.ids[this.state.i]
+
+    var homeowner_id = this.state.row["homeowner_id"]
+    var quote = this.state.row["quote"]
+    var amount = this.state.row["amountRemaining"]
+    var address  = this.state.row["address"]
+    var city = this.state.row["city"]
+    var price = this.state.row["price"]
+    var state = this.state.row["state"]
+    var zip = this.state.row["zip"]
+    //var daysRem = this.state.row["daysRemaining"]
+    var daysRem = 0
+    var updateData = {
+      homeowner_id: homeowner_id,
+      address: address,
+      city: city,
+      price: price,
+      quote: quote,
+      state: state,
+      zip: zip,
+      daysRemaining: daysRem,
+      amountRemaining: amount,
+      status: 6
+    }
+    updates['/houses/' + id] = updateData
+    var tempPlan = this.state.row
+    tempPlan = updateData
+    this.setState({row: tempPlan})
+    return firebase.database().ref().update(updates)
+  }
+
   updateHouse(index, inputValue) {
 
     var id = this.state.ids[index]
-    var inputValue_as_int = parseInt(inputValue)
-    var inputValue_as_bignumber = this.state.myweb3.toBigNumber(inputValue_as_int)
+    console.log(this.state.myweb3)
+    console.log(index)
+    console.log(inputValue)
+    var inputValue_as_bignumber = this.state.myweb3.toBigNumber(inputValue)
 
     var house_token = hash(id)
     var inputValue_as_wei = this.state.myweb3.toWei(inputValue_as_bignumber, 'ether')
